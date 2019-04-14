1f4f3e8db60f91239dc34923fb9920f202b61851
Janice Pena <jipena94@gmail.com>
2019-04-14 21:10:16 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,15 +1,16 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {
-      :patriarch
-      :matriarch
-      :hero
-      :hero_friends
-      #your key/value pairs here!
-      }, 
-    :capulet => {
-      #your key/value pairs here!
-      }}
-
+ :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+   },
+   :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+   }
+}
   
 end
